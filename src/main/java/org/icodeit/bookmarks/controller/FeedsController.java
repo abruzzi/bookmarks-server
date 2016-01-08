659b919100007e9460a5aa80f77bd528f372132b
Qiu Juntao <juntao.qiu@gmail.com>
2016-01-08 23:09:52 +1100

    add cross domain exception

--- src/main/java/org/icodeit/bookmarks/controller/FeedsController.java
+++ src/main/java/org/icodeit/bookmarks/controller/FeedsController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+
 @Controller
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://bookmarks-frontend.s3-website-us-west-2.amazonaws.com/")
 public class FeedsController {
 
     @Autowired
